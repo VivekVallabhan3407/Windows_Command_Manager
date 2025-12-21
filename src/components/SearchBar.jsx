1c757ab04eb60e6472ff2b9921b904c0734805ac
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-21 11:18:28 +0530

    Updated styling of searchbar in SearchBar.jsx file

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,22 @@
-const SearchBar=({searchtext, onSearchChange})=>{
-    return(
-        <input 
+const SearchBar = ({ searchtext, onSearchChange }) => {
+    return (
+        <input
             type="text"
             placeholder="Search commands..."
             value={searchtext}
-            onChange={(e)=>onSearchChange(e.target.value)}
-            style={
-                {
-                    padding:"8px",
-                    width:"100%",
-                    marginBottom:"20px",
-                }
-            }
+            onChange={(e) => onSearchChange(e.target.value)}
+            className="
+        w-full
+        p-3
+        border
+        border-gray-300
+        rounded-lg
+        outline-none
+        focus:ring-2
+        focus:ring-blue-500
+        focus:border-blue-500
+        text-gray-800
+      "
         />
     );
 };
