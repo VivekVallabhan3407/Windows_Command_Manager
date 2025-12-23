c5b2afbc55fe8460943f2e049c7ea20c44e6b351
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-23 21:54:55 +0530

    Updated app.jsx to import PracticeView component and add its section inside project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Sidebar from './components/SideBar';
 import HomeView from './components/HomeView';
 import ReferenceView from './components/ReferenceView';
+import PracticeView from './components/PracticeView';
 
 function App() {
   const [activeView, setActiveView] = useState("home");
@@ -15,6 +16,7 @@ function App() {
       <div style={{ flex: 1, padding: "20px" }}>
         {activeView === "home" && <HomeView setActiveView={setActiveView} />}
         {activeView === "reference" && <ReferenceView selectedCategory={selectedCategory}/>}
+        {activeView==="practice" && <PracticeView/>}
       </div>
     </div>
   )
