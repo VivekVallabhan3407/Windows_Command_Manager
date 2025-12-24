29491a6a2e2bcb1ea98086dcdf753e630c0b5c87
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-24 22:14:38 +0530

    updated PractcieView.md to add disabled style for next button when the last command is shown

--- src/components/PracticeView.jsx
+++ src/components/PracticeView.jsx
@@ -63,7 +63,8 @@ const PracticeView = () => {
 
           <button
             onClick={handleNext}
-            className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+            disabled={filteredcommands.length === 0}
+            className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 disabled:opacity-50"
           >
             Next
           </button>
