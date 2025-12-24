07a638d450624091a0d63491ab469b95c2ca1b31
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-24 22:19:37 +0530

    updated PracticeView.jsx

--- src/components/PracticeView.jsx
+++ src/components/PracticeView.jsx
@@ -63,7 +63,7 @@ const PracticeView = () => {
 
           <button
             onClick={handleNext}
-            disabled={filteredcommands.length === 0}
+            disabled={commands.length === 0}
             className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 disabled:opacity-50"
           >
             Next
